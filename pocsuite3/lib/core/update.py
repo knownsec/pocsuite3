f8d5ab5ba33b95d617d30d5e95fbb56c4aea3f80
boy-hack <34109680@qq.com>
2019-03-18 16:17:53 +0800

    修复自动更新时文件路径不正确问题

--- pocsuite3/lib/core/update.py
+++ pocsuite3/lib/core/update.py
@@ -3,7 +3,6 @@ import time
 import os
 import re
 import subprocess
-
 from pocsuite3.lib.core.common import data_to_stdout
 from pocsuite3.lib.core.common import get_latest_revision
 from pocsuite3.lib.core.common import poll_process
@@ -20,35 +19,28 @@ from pocsuite3.lib.core.settings import VERSION
 def update():
     if not conf.update_all:
         return
-
     success = False
-
-    if not os.path.exists(os.path.join(paths.POCSUITE_ROOT_PATH, ".git")) and not os.path.exists(
-            os.path.join(paths.POCSUITE_ROOT_PATH, "../", ".git")):
+    if not os.path.exists(os.path.join(paths.POCSUITE_ROOT_PATH, "../", ".git")):
         warn_msg = "not a git repository. It is recommended to clone the 'knownsec/pocsuite3' repository "
         warn_msg += "from GitHub (e.g. 'git clone --depth 1 {} pocsuite3')".format(GIT_REPOSITORY)
         logger.warn(warn_msg)
-
         if VERSION == get_latest_revision():
             logger.info("already at the latest revision '{}'".format(get_revision_number()))
             return
-
     else:
         info_msg = "updating pocsuite3 to the latest development revision from the "
         info_msg += "GitHub repository"
         logger.info(info_msg)
-
         debug_msg = "pocsuite3 will try to update itself using 'git' command"
         logger.debug(debug_msg)
-
         data_to_stdout("\r[{0}] [INFO] update in progress ".format(time.strftime("%X")))
-
+        cwd_path = os.path.join(paths.POCSUITE_ROOT_PATH, "../")
         try:
             process = subprocess.Popen("git checkout . && git pull %s HEAD" % GIT_REPOSITORY,
                                        shell=True,
                                        stdout=subprocess.PIPE,
                                        stderr=subprocess.PIPE,
-                                       cwd=paths.SQLMAP_ROOT_PATH.encode(
+                                       cwd=cwd_path.encode(
                                            sys.getfilesystemencoding() or UNICODE_ENCODING))
             poll_process(process, True)
             stdout, stderr = process.communicate()
@@ -56,9 +48,8 @@ def update():
         except (IOError, OSError) as ex:
             success = False
             stderr = str(ex)
-
         if success:
-            logger.info("{0} the latest revision '{1}'".format("already at" if "Already" in stdout else "updated to",
+            logger.info("{0} the latest revision '{1}'".format("already at" if b"Already" in stdout else "updated to",
                                                                get_revision_number()))
         else:
             if "Not a git repository" in stderr:
@@ -67,7 +58,6 @@ def update():
                 logger.error(err_msg)
             else:
                 logger.error("update could not be completed ('%s')" % re.sub(r"\W+", " ", stderr).strip())
-
     if not success:
         if IS_WIN:
             info_msg = "for Windows platform it's recommended "
@@ -78,5 +68,4 @@ def update():
         else:
             info_msg = "for Linux platform it's recommended "
             info_msg += "to install a standard 'git' package (e.g.: 'sudo apt-get install git')"
-
         logger.info(info_msg)
