60ec2ecb7f3a0bdc8aebd567dd5b20aecfe9961c
ro0tk1t <1576235794@qq.com>
2021-01-18 09:55:50 +0800

    fix: isAlive()
    close #154

--- pocsuite3/lib/core/threads.py
+++ pocsuite3/lib/core/threads.py
@@ -66,7 +66,7 @@ def run_threads(num_threads, thread_function, args: tuple = (), forward_exceptio
         while alive:
             alive = False
             for thread in threads:
-                if thread.isAlive():
+                if thread.is_alive():
                     alive = True
                     time.sleep(0.1)
 
