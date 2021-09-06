0ecf2b030d3a3fdc0766724f9682bbe463cfd74c
Tian Qiao <abcnsxyz@gmail.com>
2021-09-06 14:00:37 +0800

    chore(): update dependency

--- setup.py
+++ setup.py
@@ -36,17 +36,16 @@ Pocsuite is an open-sourced remote vulnerability testing and proof-of-concept de
         ]
     },
     install_requires=[
-        "requests >= 2.22.0",
-        "PySocks >= 1.7.1",
-        "requests-toolbelt >= 0.9.1",
-        "urllib3 >= 1.25.6",
-        "setuptools >= 51.1.2",
-        "chardet >= 3.0.4",
-        "termcolor >= 1.1.0",
-        "colorama >= 0.4.4",
-        "prettytable >= 0.7.2",
-        "colorlog >= 4.7.2",
-        "scapy >= 2.4.4"
+        "requests",
+        "requests-toolbelt",
+        "PySocks",
+        "urllib3",
+        "chardet",
+        "termcolor",
+        "colorama",
+        "prettytable",
+        "colorlog",
+        "scapy"
     ],
     extras_require={
         'complete': [
