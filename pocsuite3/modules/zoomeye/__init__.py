62c7ed20cb895905d098d3a41cebeb91e276a1df
Tian Qiao <abcnsxyz@gmail.com>
2021-12-08 21:59:43 +0800

    update zoomeye module

--- pocsuite3/modules/zoomeye/__init__.py
+++ pocsuite3/modules/zoomeye/__init__.py
@@ -9,7 +9,7 @@ from pocsuite3.lib.request import requests
 
 class ZoomEye():
     def __init__(self, conf_path=paths.POCSUITE_RC_PATH, username=None, password=None):
-        self.headers = None
+        self.headers = {'User-Agent': 'curl/7.80.0'}
         self.token = None
         self.resources = None
         self.plan = None
@@ -20,23 +20,22 @@ class ZoomEye():
         if self.conf_path:
             self.parser = ConfigParser()
             self.parser.read(self.conf_path)
-
-        if username and password:
-            self.check_account()
-        else:
             try:
                 self.token = self.parser.get("Telnet404", 'Jwt token')
             except Exception:
                 pass
 
+        self.check_account()
+
     def token_is_available(self):
         if self.token:
-            headers = {'Authorization': 'JWT %s' % self.token}
+            self.headers['Authorization'] = f'JWT {self.token}'
             try:
-                resp = requests.get('https://api.zoomeye.org/resources-info', headers=headers)
-                if resp and resp.status_code == 200 and "plan" in resp.json():
-                    self.headers = headers
+                resp = requests.get('https://api.zoomeye.org/resources-info', headers=self.headers)
+                if resp and resp.status_code == 200 and "plan" in resp.text:
                     return True
+                else:
+                    logger.info(resp.text)
             except Exception as ex:
                 logger.error(str(ex))
         return False
@@ -44,12 +43,14 @@ class ZoomEye():
     def new_token(self):
         data = '{{"username": "{}", "password": "{}"}}'.format(self.username, self.password)
         try:
-            resp = requests.post('https://api.zoomeye.org/user/login', data=data, )
-            if resp.status_code != 401 and "access_token" in resp.json():
+            resp = requests.post('https://api.zoomeye.org/user/login', data=data)
+            if resp.status_code != 401 and "access_token" in resp.text:
                 content = resp.json()
                 self.token = content['access_token']
-                self.headers = {'Authorization': 'JWT %s' % self.token}
+                self.headers['Authorization'] = f'JWT {self.token}'
                 return True
+            else:
+                logger.info(resp.text)
         except Exception as ex:
             logger.error(str(ex))
         return False
@@ -57,24 +58,21 @@ class ZoomEye():
     def check_account(self):
         if self.token_is_available():
             return True
-        else:
-            if self.username and self.password:
-                if self.new_token():
-                    self.write_conf()
-                    return True
+        elif self.username and self.password:
+            if self.new_token():
+                self.write_conf()
+                return True
+        while True:
+            username = input("Telnet404 email account: ")
+            password = getpass.getpass("Telnet404 password: (input will hidden)")
+            self.username = username
+            self.password = password
+            if self.new_token():
+                self.write_conf()
+                return True
             else:
-                username = input("Telnet404 email account:")
-                password = getpass.getpass("Telnet404 password:")
-                self.username = username
-                self.password = password
-                if self.new_token():
-                    self.write_conf()
-                    return True
-                else:
-                    logger.error("The username or password is incorrect. "
-                                 "Please enter the correct username and password.")
-                    return False
-        return False
+                logger.error("The username or password is incorrect, "
+                             "Please enter the correct username and password.")
 
     def write_conf(self):
         if not self.parser.has_section("Telnet404"):
@@ -89,7 +87,7 @@ class ZoomEye():
         if self.check_account():
             try:
                 resp = requests.get('https://api.zoomeye.org/resources-info', headers=self.headers)
-                if resp and resp.status_code == 200 and 'plan' in resp.json():
+                if resp and resp.status_code == 200 and 'plan' in resp.text:
                     content = resp.json()
                     self.plan = content['plan']
                     self.resources = content['resources']['search']
@@ -104,11 +102,11 @@ class ZoomEye():
             kb.comparison.add_dork("Zoomeye", dork)
         try:
             for page in range(1, pages + 1):
-                url = "https://api.zoomeye.org/{}/search?query={}&page={}&facet=app,os".format(resource,
-                                                                                               urllib.parse.quote(dork),
-                                                                                               page)
+                url = (
+                    "https://api.zoomeye.org/{}/search?query={}&page={}&facet=app,os"
+                ).format(resource, urllib.parse.quote(dork), page)
                 resp = requests.get(url, headers=self.headers)
-                if resp and resp.status_code == 200 and "matches" in resp.json():
+                if resp and resp.status_code == 200 and "matches" in resp.text:
                     content = resp.json()
                     if resource == 'web':
                         for match in content["matches"]:
