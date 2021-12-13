4856f47ce5952f9cc188891b0ee29543734c19f2
13ph03nix <abcnsxyz@gmail.com>
2021-12-13 17:26:25 +0800

    refactor: rewrite censys module (#249)

--- pocsuite3/modules/censys/__init__.py
+++ pocsuite3/modules/censys/__init__.py
@@ -1,13 +1,12 @@
-import json
+import getpass
 from configparser import ConfigParser
-
-import requests
-
 from pocsuite3.lib.core.data import paths, logger
+from pocsuite3.api import requests
 
 
 class Censys():
     def __init__(self, conf_path=paths.POCSUITE_RC_PATH, uid='', secret=''):
+        self.headers = {'User-Agent': 'curl/7.80.0', 'Accept': 'application/json'}
         self.uid = uid
         self.secret = secret
         self.conf_path = conf_path
@@ -15,21 +14,26 @@ class Censys():
         if self.conf_path:
             self.parser = ConfigParser()
             self.parser.read(self.conf_path)
-            if not (self.secret or self.uid):
-                try:
-                    self.secret = self.parser.get("Censys", "secret")
-                    self.uid = self.parser.get("Censys", "uid")
-                except Exception:
-                    pass
+            try:
+                self.uid = self.uid or self.parser.get("Censys", "uid")
+                self.secret = self.secret or self.parser.get("Censys", "secret")
+            except Exception:
+                pass
+
+        self.check_token()
 
     def token_is_available(self):
         if self.secret and self.uid:
             try:
-                resp = requests.get("https://censys.io/api/v1/account", auth=(self.uid, self.secret))
-                if resp.status_code == 200 and "email" in resp.json():
-                    logger.info("[PLUGIN] Censys login success email:{}".format(resp.json()["email"]))
-                    self.credits = resp.json()["quota"]["allowance"] - resp.json()["quota"]["used"]
+                resp = requests.get("https://search.censys.io/api/v1/account", auth=(self.uid, self.secret),
+                                    headers=self.headers)
+                if resp.status_code == 200 and 'allowance' in resp.text:
+                    js = resp.json()
+                    logger.info("[PLUGIN] Censys login success, email: {}".format(js["email"]))
+                    self.credits = js["quota"]["allowance"] - js["quota"]["used"]
                     return True
+                else:
+                    logger.info(resp.text)
             except Exception as ex:
                 logger.error(ex)
         return False
@@ -37,18 +41,17 @@ class Censys():
     def check_token(self):
         if self.token_is_available():
             return True
-        else:
-            new_UID = input("Censys API UID:")
-            new_secret = input("Censys API SECRET")
+        while True:
+            new_UID = input("Censys API ID: ")
+            new_secret = getpass.getpass("Censys API SECRET: (input will hidden)")
             self.uid = new_UID
             self.secret = new_secret
             if self.token_is_available():
                 self.write_conf()
                 return True
             else:
-                logger.error("The shodan api token is incorrect. "
-                             "Please enter the correct api token.")
-                self.check_token()
+                logger.error("The censys api id or secret are incorrect, "
+                             "Please enter a correct one.")
 
     def write_conf(self):
         if not self.parser.has_section("Censys"):
@@ -68,19 +71,25 @@ class Censys():
     def search(self, dork, pages=1, resource='ipv4'):
         search_result = set()
         try:
+            cursor = ''
             for page in range(1, pages + 1):
-                url = "https://censys.io/api/v1/search/{}".format(resource)
+                url = "https://search.censys.io/api/v2/hosts/search"
                 data = {
-                    "query": dork,  # Search keywords,
-                    "fields": ["ip"],
-                    "page": page
+                    "q": dork,  # Search keywords
+                    "per_page": 50,
+                    "virtual_hosts": "EXCLUDE"
                 }
-                resp = requests.post(url, data=json.dumps(data), auth=(self.uid, self.secret))
-                if resp and resp.status_code == 200 and "results" in resp.json():
-                    content = resp.json()["results"]
-                    for match in content:
-                        ans = match["ip"]
-                        search_result.add(ans)
+                if cursor:
+                    data['cursor'] = cursor
+                resp = requests.get(url, params=data, auth=(self.uid, self.secret), headers=self.headers)
+                if resp and resp.status_code == 200 and 'result' in resp.json():
+                    results = resp.json()['result']['hits']
+                    cursor = resp.json()['result']['links']['next']
+                    for i in results:
+                        ip = i['ip']
+                        for j in i['services']:
+                            port = j['port']
+                            search_result.add(f'{ip}:{port}')
         except Exception as ex:
             logger.error(str(ex))
         return search_result
@@ -88,4 +97,6 @@ class Censys():
 
 if __name__ == '__main__':
     c = Censys()
-    ret = c.search("apache")
+    ret = c.search("thinkphp", pages=2)
+    for i in ret:
+        print(i)
