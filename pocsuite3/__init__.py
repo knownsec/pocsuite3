72da6ac648041e048fb59b0ea2713ef328b479e4
Tian Qiao <abcnsxyz@gmail.com>
2022-03-04 19:36:17 -0800

    chore: bump version to 1.9.0

--- pocsuite3/__init__.py
+++ pocsuite3/__init__.py
@@ -1,9 +1,9 @@
 __title__ = 'pocsuite3'
-__version__ = '1.8.12'
+__version__ = '1.9.0'
 __author__ = 'Knownsec 404 Team'
 __author_email__ = '404-team@knownsec.com'
 __license__ = 'GPLv2'
-__copyright__ = 'Copyright 2014-2021 Knownsec 404 Team'
+__copyright__ = 'Copyright 2014-2022 Knownsec 404 Team'
 __name__ = 'pocsuite3'
 __package__ = 'pocsuite3'
 
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ long_description = (
 
 setup(
     name='pocsuite3',
-    version='1.8.12',
+    version='1.9.0',
     url='https://pocsuite.org',
     description='Open-sourced remote vulnerability testing framework.',
     long_description=long_description,
