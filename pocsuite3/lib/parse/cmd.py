ff61b4596fc7a8aa0a0849752dfabf39bc99f0da
13ph03nix <17541483+13ph03nix@users.noreply.github.com>
2023-03-20 22:45:06 -0700

    feat: support disable protocol correction and honeypot checks

--- pocsuite3/lib/parse/cmd.py
+++ pocsuite3/lib/parse/cmd.py
@@ -150,6 +150,8 @@ def cmd_line_parser(argv=None):
                                   help="only export request rule")
         optimization.add_argument("--rule-filename", dest="rule_filename", action="store", default=False,
                                   help="Specify the name of the export rule file")
+        optimization.add_argument("--no-check", dest="no_check", action="store_true", default=False,
+                                  help="Disable URL protocol correction and honeypot check")
         # Diy options
         diy = parser.add_argument_group("Poc options", "definition options for PoC")
         diy.add_argument("--options", dest="show_options", action="store_true", default=False,
